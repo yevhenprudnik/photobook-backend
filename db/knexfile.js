58c0e204c9d22828d90e278348426975a915b3b9
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-10-30 22:02:38 +0200

    Fix migration script.

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,14 +1,15 @@
-import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from '../environment';
+import * as env from 'dotenv';
+env.config({ path: '../.env' });
 
 export default {
   development: {
     client: 'pg',
     connection: {
-      host: DB_HOST,
-      port: DB_PORT,
-      user: DB_USER,
-      password: DB_PASS,
-      database: DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
     },
   },
 };
