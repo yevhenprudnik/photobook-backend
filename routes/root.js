499b4936f0c7febbcae0009c1ab45a98802912ca
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-11-27 22:02:52 +0200

    remove testing route.

--- routes/root.js
+++ routes/root.js
@@ -3,8 +3,4 @@ export default async server => {
   server.get('/', async (request, reply) => {
     return { root: true };
   });
-
-  server.get('/fly', async (request, reply) => {
-    return { fly: true };
-  });
 };
