16dfa02cc6de31719b9d5f5868f13cee0fece45e
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-11-27 23:15:14 +0200

    Final test.

--- routes/root.js
+++ routes/root.js
@@ -3,4 +3,8 @@ export default async server => {
   server.get('/', async (request, reply) => {
     return { root: true };
   });
+
+  server.get('/fly', async (request, reply) => {
+    return { fly: true };
+  });
 };
